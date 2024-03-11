359e9832b7fddca8263dbdc0a944355f69cd9432
hamza5645 <hamzaosama5645@gmail.com>
2024-03-11 12:34:59 +0200

    Initial Commit

---
+++ Sticky Note/Sticky_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sticky_NoteApp.swift
+//  Sticky Note
+//
+//  Created by Hamza Osama on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sticky_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
